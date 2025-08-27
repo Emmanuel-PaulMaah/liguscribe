e184838806db2ebc3b4150d250dc58bb26a0bd6c
Emmanuel-PaulMaah <emmanuel.paulmaah@gmail.com>
2025-08-27 04:51:49 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'LiguScribe – Court Transcription Software',
+  description: 'Courtroom Transcription Software',
+  author: 'Emmanuel PaulMaah',
 }
 
 export default function RootLayout({
